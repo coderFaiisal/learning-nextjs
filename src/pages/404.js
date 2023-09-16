f5b60b53e00fc3499f97cc6ebcefbabbf746eb28
Faisal <officialfaisal1112@gmail.com>
2023-09-16 23:20:31 +0600

    implement error route and slug page

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>404 error!!! Something went wrong...!</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
---
+++ src/pages/products/[...slug].js
@@ -0,0 +1,9 @@
+const FilteringPage = () => {
+  return (
+    <div>
+      <h1>This is filering page...</h1>
+    </div>
+  );
+};
+
+export default FilteringPage;
