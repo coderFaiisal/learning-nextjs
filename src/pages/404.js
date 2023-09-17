3c0ee627349ed70ea02ea2c69ae7dc53a8f482eb
Faisal <officialfaisal1112@gmail.com>
2023-09-17 10:39:03 +0600

    implement navigation and programmatically navigation

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,16 @@
+import { useRouter } from "next/router";
+
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>404 error!!! Something went wrong...!</h1>
-        </div>
-    );
+  const router = useRouter();
+
+  setTimeout(() => {
+    router.push("/");
+  }, 4000);
+  return (
+    <div>
+      <h1>404 error!!! Something went wrong...!</h1>
+    </div>
+  );
 };
 
 export default ErrorPage;
--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,6 +1,12 @@
+import { Button } from "antd";
+import Link from "next/link";
+
 const PostsHomePage = () => {
   return (
     <div>
+      <Button type="primary">
+        <Link href="/">Home</Link>
+      </Button>
       <h1>This is posts home page...</h1>
     </div>
   );
