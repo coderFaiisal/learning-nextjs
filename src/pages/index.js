06510e840ff71987d1be9f281ce852a46e6b39df
Faisal <officialfaisal1112@gmail.com>
2023-09-18 18:45:12 +0600

    implement seo tags and image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import RootLayout from "../../components/Layouts/RootLayout";
+import Image from "next/image";
+import Head from "next/head";
 
 const HomePage = () => {
   return (
     <div>
+      <Head>
+        <title>Next App Home</title>
+        <meta name="Home page" description="This is description of next app" />
+      </Head>
       <h1>This is home page...</h1>
+      <Image
+        src="https://images.pexels.com/photos/9638689/pexels-photo-9638689.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+        alt="image"
+        width={500}
+        height={500}
+        layout="responsive"
+      />
     </div>
   );
 };
